60166a22b1bc0dd6299abca05a60ea8b3869e6fc
Kirill Mokhov <mkir05@mail.ru>
2023-01-29 19:06:54 +0300

    похуй

--- draft.py
+++ draft.py
@@ -1,2 +1,11 @@
 # Тут всякое говно можно удалять/изменять
-print(1.1198/365.25)
+import fc
+import datetime
+# a, b, c = '2024,01,01'.rsplit(',')
+# print(a)
+# print(b)
+# print(c)
+a,a1,a2 = input("первая дата").rsplit(",")
+b,b1,b2 = input("вторая дата").rsplit(",")
+c = fc.get_days(datetime.date(int(a), int(a1), int(a2)), datetime.date(int(b), int(b1), int(b2)))
+print(c)
